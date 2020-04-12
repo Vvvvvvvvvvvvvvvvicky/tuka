3ecc3d59c486c4882979d453388ac2ce49a3deef
vic <vicky.h.m.lee@outlook.com>
2020-04-12 22:34:49 +0800

    update

--- pages/daily/daily.js
+++ pages/daily/daily.js
@@ -4,7 +4,36 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    dailyRecommend:{
+    url:"http://image.xingpin.com/img/pximg/201912/20/3/7a2721576826601.jpg",
+    reason:"浪漫的跨年时刻即将到来，你准备怎么过？时尚达人黄灿灿为你揭晓COACH心动单品，飘逸的印花长裙让她散发出俏丽迷人的姿态，经典标志帆布DREAMER斜挎包轻倚身侧，让优雅恰到好处。当复古优雅与风格酷范碰撞，穿行于街头，举手投足间都散发着优雅韵味。官网更有超多款单品优惠，前往COACH官网，探寻惊喜！"
+    },
+    dailyDetailList:[
+      {
+        detailUrl:"http://image.xingpin.com/./img/uimg/201912/20/3/uyjdd20191220152144_90.png",
+        name:"COACH 拼色经典标志帆布DREAMER多功能斜挎包",
+        pricePre:3500,
+        priceNow:2240
+      },
+      {
+        detailUrl: "http://image.xingpin.com/./img/uimg/201912/20/3/gwicw20191220152109_90.png",
+        name: "COACH 系带链条短靴",
+        pricePre: 5950,
+        priceNow: 2975
+      },
+      {
+        detailUrl: "http://image.xingpin.com/./img/uimg/201912/20/3/ozjgr20191220152030_90.png",
+        name: "COACH 大尺寸皮毛一体飞行员夹克",
+        pricePre: 21000,
+        priceNow: 14700
+      },
+      {
+        detailUrl: "http://image.xingpin.com/./img/uimg/201912/20/3/khksu20191220151957_90.png",
+        name: "COACH KAFFE FASSETT印花百褶连衣裙",
+        pricePre: 6500,
+        priceNow: 3250
+      }
+    ]
   },
 
   /**
